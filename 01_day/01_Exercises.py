bf3520b219fdcbfddef0f721de19e9e7ea1398b6
UP240101 <up240101@alumnos.upa.edu.mx>
2025-02-28 20:53:32 -0600

    Primer commit

---
+++ 01_day/01_Exercises.py
@@ -0,0 +1,25 @@
+"""
+Open the python interactive shell and do the following operations.
+The operands are 3 and 4.
+"""
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3%4)
+print(3**4)
+print(3//4)
+#Write strings on the python interactive shell
+print("sergio")
+print("martinez")
+print("mexico")
+print("I am enjoying 30 days of python")
+#verificacion de los tipos de datos
+print(type(10))                           
+print(type(9.8))                          
+print(type(3.14))                         
+print(type(4 - 4j))                       
+print(type(['Asabeneh', 'Python', 'Finland']))  
+print(type("Sergio"))                
+print(type("Martinez Castillo"))           
+print(type("Mexico"))               
